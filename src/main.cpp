a35bc2c5f1d598c124e247ceb7e5985fb1bcbf23
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-04-13 23:09:22 +0300

    Code for buttons and ultrasonic senzor

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,9 @@
 #define PIN_SPEAKER 9
 // For ultrasonic sensors:
 #define PIN_TRIGGER 8
-#define PIN_ECHO 7
+#define PIN_ECHO_1 7
+#define PIN_ECHO_2 6
+
 
 /*
 song01.wav - Roman Jewels - Rolling
@@ -29,7 +31,6 @@ TMRpcm audio;
 volatile int volume = 4;
 volatile bool isPausePlayButtonPressed = false;
 
-long duration, cm, inches;
 
 void decreaseVolume()
 {
@@ -51,18 +52,17 @@ void play_pause()
 
 void nextSong()
 {
-  song_index = (song_index + 1) % 4 + 1;
+  song_index = (song_index + 1) % 4;
   
-  if (song_index == 1) {
+  if (song_index == 0) {
     audio.play("songs/song01.wav");
-  } else if (song_index == 2) {
+  } else if (song_index == 1) {
     audio.play("songs/song02.wav");
-  } else if (song_index == 3) {
+  } else if (song_index == 2) {
     audio.play("songs/song03.wav");
-  } else if (song_index == 4) {
+  } else if (song_index == 3) {
     audio.play("songs/song04.wav");
   }
-
 }
 
 
@@ -76,7 +76,8 @@ void setup() {
   pinMode(PIN_BUTTON_NEXT_SONG, INPUT_PULLUP);
 
   pinMode(PIN_TRIGGER, OUTPUT);
-  pinMode(PIN_ECHO, INPUT);
+  pinMode(PIN_ECHO_1, INPUT);
+  pinMode(PIN_ECHO_2, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(PIN_BUTTON_VOLUME_DOWN), decreaseVolume, FALLING);
   attachInterrupt(digitalPinToInterrupt(PIN_BUTTON_VOLUME_UP), increaseVolume, FALLING); 
@@ -93,7 +94,7 @@ void setup() {
 
   Serial.println("Playing audio...");
 
-  song_index = 1;
+  song_index = 0;
   audio.play("songs/song01.wav");
   audio.setVolume(volume);
 
@@ -111,20 +112,48 @@ void loop() {
   digitalWrite(PIN_TRIGGER, HIGH);
   delayMicroseconds(10);
   digitalWrite(PIN_TRIGGER, LOW);
+  long duration1 = pulseIn(PIN_ECHO_1, HIGH);
+
+  digitalWrite(PIN_TRIGGER, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PIN_TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIGGER, LOW);
+  long duration2 = pulseIn(PIN_ECHO_2, HIGH);
+
+
  
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(PIN_ECHO, INPUT);
-  duration = pulseIn(PIN_ECHO, HIGH);
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
+  long distance1 = (duration1/2) / 29.1;
+  long distance2 = (duration2/2) / 29.1;
+
+  /*
+  Serial.print("Distance senzor 1: ");
+  Serial.print(distance1);
+  Serial.print("cm; distance senzor 2: ");
+  Serial.print(distance2);
   Serial.print("cm");
   Serial.println();
-  delay(100);
+  */
+
+  if (digitalRead(PIN_BUTTON_PLAY_PAUSE) == LOW) {
+    audio.pause();
+  }
+  if (digitalRead(PIN_BUTTON_NEXT_SONG) == LOW) {
+    song_index = (song_index + 1) % 4;
+  
+    if (song_index == 0) {
+      audio.play("songs/song01.wav");
+    } else if (song_index == 1) {
+      audio.play("songs/song02.wav");
+    } else if (song_index == 2) {
+      audio.play("songs/song03.wav");
+    } else if (song_index == 3) {
+      audio.play("songs/song04.wav");
+    }
+
+  }
+
+
+
+  delay(50);
 }
